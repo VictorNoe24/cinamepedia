879d5fe05f709c68f39eca27b842ef4651c58dfc
VictorNoe24 <noeflo60@gmail.com>
2024-09-21 10:35:52 -0600

    [ADD] api

---
+++ lib/infrastructure/repositories/movie_respository_impl.dart
@@ -0,0 +1,16 @@
+import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/domain/repositories/movies_respository.dart';
+
+class MovieRespositoryImpl extends MoviesRepository {
+
+  final MoviesDatasource datasource;
+
+  MovieRespositoryImpl(this.datasource);
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) {
+    return datasource.getNowPlaying(page: page);
+  }
+
+}
